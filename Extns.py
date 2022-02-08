f48573430c72f88bf996ea6923fda1261c9e06e2
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2022-02-08 12:14:59 +0530

    Update extns.py

--- Extns.py
+++ Extns.py
@@ -1,18 +1,4 @@
-filename = input("Enter the filename : ");
-f_exts=filename.split(".");
-print("The extension used here is "+repr(f_exts[-1]));
-if(f_exts == py)
-{ print("This is file of Python programe ");
-}
-else if(f_exts == java)
-{ print("file is of Java programe ");
-}
-else if(f_exts == c)
-{ print("file is of C-programe ");
-}
-else if(f_exts == cpp)
-{ print("file is of C++ programe ");
-}
-else
-{ print("invalid extension ");
-}
+#file extension
+filename=input("Enter the filename : ")
+f_exts=filename.split(".")
+print("file extension is ",f_exts)
