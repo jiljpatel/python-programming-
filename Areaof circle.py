15f0c46839ef92d448b7f0e294ab4ec0a25f0ef2
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2022-02-08 12:13:47 +0530

    Update circle.py

--- Areaof circle.py
+++ Areaof circle.py
@@ -1,5 +1,5 @@
-#Area of circle
-pi=3.14; 
-radius= float(input("Enter the radius of circle : "));
-area=pi*radius**2;
-print("Area of circle with given radius is = ");
+#Areaof circle 
+radius = float(input("Enter the radius of circle : "))
+pi=3.14
+area=pi*radius**2
+print("area of circle with given radius is: ",area)
