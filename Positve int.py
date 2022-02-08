027204b232428815b5a68dd01761f56c6aa899ef
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2022-02-08 16:00:10 +0530

    Create positive int.py

---
+++ Positve int.py
@@ -0,0 +1,14 @@
+#List of positive integers
+list1 = [12,-7,4,5,-14,8]
+print(list1) 
+print("positive integers are as follows ")
+for n in list1:
+If(n>0)
+print(n)
+
+list2=[12,14,-95,3,-78]
+print(list2)
+print("positive integers are as follows ")
+for n in list2:
+If(n>0)
+print(n)
