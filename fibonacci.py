3516abbdfacfdd1cbf0b34107d9a9f33e0d1755b
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2022-02-12 11:15:38 +0530

    Createfibo.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,11 @@
+
 #fibonacci series
 term = int(input("Enter the term uptill u want the fibonacci series : ")) 
-n1, n2 = 0, 1
+n1=0
+n2=1
 i=0 
 while i<term: 
-print(n1) 
-n = n1+n2 
-n1=n2 
-n2=n i+=1
+     print(i) 
+     n1=n2 
+     n2=i
+     i=n1+n2
