71c4eea77da231617fe7bc5e37a4b861296ae4d0
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2022-02-22 16:00:24 +0530

    Createassignment2.1.py

---
+++ Assignment2.1.py
@@ -0,0 +1,15 @@
+#change in List
+l1=["Tony","Steve","Joseph","Baton"]
+print(l1)
+l1[2]="Banner"
+print("new list")
+print(l1)
+
+
+
+
+#Change in Dictionary
+d1={"stark":"Genius","rogger":"Warrior","loki":"devil"}
+print(d1)
+d1["loki"]= "adopted"
+print(d1)
