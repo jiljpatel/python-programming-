c51dca961e95bc06634f5ad393164e14ecd2aa8c
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2022-02-22 15:56:03 +0530

    Createass1.1.py

---
+++ Assignment1.1.py
@@ -0,0 +1,66 @@
+#use of different operators
+a=33
+b=54
+####################     Arithmatic Operators     #################
+sum=a+b
+print(sum)
+
+sub=a-b
+print(sub)
+
+pro=a*b
+print(pro)
+
+div=a/b
+print(div)
+
+sq=a**2
+print(sq)
+
+c=a//2
+print(c)
+
+###################       Assignment Operators    ################
+x=8
+y=5
+x+=2
+print(x)
+
+y-=3
+print(y)
+
+x*=5
+print(x)
+y/=2  
+print(y)
+
+##################      Comparision Operators      ################
+w=a<b
+print(w)
+u=x==y
+print(u)
+x<=a
+y>=b
+
+
+#################        Logical operators        ################
+g=a==b and x>=y
+print(g)
+a>x and b>=y
+h=a==b or x>=y
+print(h)
+
+###############         Membership Operators      #################
+a=[1,22,45,10]
+t=3 in a
+print(t)
+
+
+
+
+#********************    Algorithm     ************************
+step 1: start
+step 2:a=10
+step 3:b=1
+step 4:a-2
+step 5:b+2
